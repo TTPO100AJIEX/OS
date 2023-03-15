08faa875c0349c94275ef16c281717fdd7363249
Александр Абрамов <ttpo100ajiex@mail.ru>
2023-03-15 18:36:56 +0300

    Changed buffer size

--- IHW-1/compile.sh
+++ IHW-1/compile.sh
@@ -2,18 +2,18 @@ options="-O2 -fsanitize=address,undefined,leak -fno-sanitize-recover=all -std=c1
 
 if [ $1 -lt 8 ]
 then
-    gcc -Ilib -DCHUNK_SIZE=5000 $options lib/pipes/solve.c lib/pipes/transfer.c $1/index.c -o $1/bin/index.exe
+    gcc -Ilib -DCHUNK_SIZE=4000 $options lib/pipes/solve.c lib/pipes/transfer.c $1/index.c -o $1/bin/index.exe
 fi
 
 if [ $1 -eq 8 ] || [ $1 -eq 9 ]
 then
     if [ $1 -eq 8 ]
     then
-        chunksize=5000
+        chunksize=4000
     fi
     if [ $1 -eq 9 ]
     then
-        chunksize=200
+        chunksize=100
     fi
 
     gcc -Ilib -DCHUNK_SIZE=$chunksize $options lib/pipes/solve.c lib/pipes/transfer.c $1/io.c -o $1/bin/io.exe
@@ -22,6 +22,6 @@ fi
 
 if [ $1 -eq 10 ]
 then
-    gcc -Ilib -DCHUNK_SIZE=200 $options lib/messages/input.c lib/messages/solve.c lib/messages/output.c $1/io.c -o $1/bin/io.exe
-    gcc -Ilib -DCHUNK_SIZE=200 $options lib/messages/input.c lib/messages/solve.c lib/messages/output.c $1/solver.c -o $1/bin/solver.exe
+    gcc -Ilib -DCHUNK_SIZE=100 $options lib/messages/input.c lib/messages/solve.c lib/messages/output.c $1/io.c -o $1/bin/io.exe
+    gcc -Ilib -DCHUNK_SIZE=100 $options lib/messages/input.c lib/messages/solve.c lib/messages/output.c $1/solver.c -o $1/bin/solver.exe
 fi
